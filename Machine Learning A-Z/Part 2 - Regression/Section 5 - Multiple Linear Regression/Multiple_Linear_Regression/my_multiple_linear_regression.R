417fc65cc0eea436365be5eed6f641476dd25a1a
ub3rpyr0 <scott.wenzl@gmail.com>
2017-11-17 10:18:28 -0700

    update my multiple linear regression finish preprocessing and fit regressor

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.R
@@ -20,21 +20,26 @@ summary(dataset)
 #                         dataset$Salary)
 
 # Encoding categorical data
-dataset$Country = factor(dataset$Country,
-                         levels = c("France","Spain","Germany"),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased,
-                         levels = c("No","Yes"),
-                         labels = c(0, 1))
+dataset$State = factor(dataset$State,
+                       levels = c("New York","California","Florida"),
+                       labels = c(1, 2, 3))
 
 # Splitting the dataset into the Training set and Test set
 # install.packages("caTools")
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio=0.8)
+split = sample.split(dataset$Profit, SplitRatio=0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set[,2:3] = scale(training_set[,2:3])
 # test_set[,2:3] = scale(test_set[,2:3])
+
+
+
+# Fitting multiple linear regression to the training set
+# regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State)
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+summary(regressor)
