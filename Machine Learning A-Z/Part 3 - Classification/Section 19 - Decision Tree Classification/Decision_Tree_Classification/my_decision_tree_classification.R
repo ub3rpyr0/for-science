f20c4afa19ad2687e97239b85086117a59d6706f
ub3rpyr0 <scott.wenzl@gmail.com>
2017-11-29 11:01:20 -0700

    update my decision tree classification with plot of decision tree

--- Machine Learning A-Z/Part 3 - Classification/Section 19 - Decision Tree Classification/Decision_Tree_Classification/my_decision_tree_classification.R
+++ Machine Learning A-Z/Part 3 - Classification/Section 19 - Decision Tree Classification/Decision_Tree_Classification/my_decision_tree_classification.R
@@ -74,3 +74,7 @@ plot(set[, -3],
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Plotting the decision tree: done by importing data WITHOUT feature scaling
+plot(classifier)
+text(classifier)
