c35973eff5942b1ebb8161fd730ac0f086eda096
ub3rpyr0 <scott.wenzl@gmail.com>
2017-11-29 11:27:24 -0700

    add material for random forest classification and add and complete my random forest classification R script

---
+++ Machine Learning A-Z/Part 3 - Classification/Section 20 - Random Forest Classification/Random_Forest_Classification/my_random_forest_classification.R
@@ -0,0 +1,77 @@
+# Random Forest Classification
+
+# Importing the dataset
+dataset = read.csv("Social_Network_Ads.csv")
+dataset = dataset[3:5] # use this to select a subset of the data
+
+# Taking care of missing data
+# dataset$Age = ifelse(is.na(dataset$Age),
+#                      ave(dataset$Age, FUN=function(x) mean(x,na.rm=TRUE)),
+#                      dataset$Age)
+# dataset$Salary = ifelse(is.na(dataset$Salary),
+#                         ave(dataset$Salary, FUN=function(x) mean(x,na.rm=TRUE)),
+#                         dataset$Salary)
+
+# Encoding categorical data
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio=0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+
+# Fitting Random Forest to the Training set
+# install.packages('randomForest')
+library(randomForest)
+classifier = randomForest(x = training_set[-3],
+                          y = training_set$Purchased,
+                          ntree = 10)
+
+# Predicting the Test set results
+y_pred=predict(classifier,newdata=test_set[-3])
+
+# Making the Confusion Matrix - tabulates number of correct and incorrect predictions
+cm = table(test_set[,3], y_pred)
+
+# Visualizing the Training set results
+# install.packages("ElemStatLearn")
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age','EstimatedSalary')
+y_grid = predict(classifier, newdata=grid_set)
+plot(set[, -3],
+     main = 'Random Forest (Training Set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualizing the Test set results
+# install.packages("ElemStatLearn")
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age','EstimatedSalary')
+y_grid = predict(classifier, newdata=grid_set)
+plot(set[, -3],
+     main = 'Random Forest (Test Set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
