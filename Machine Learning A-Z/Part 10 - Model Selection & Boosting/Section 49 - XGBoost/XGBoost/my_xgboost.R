4c1d1fddb9e709f471d5a0e55bee714a7e827856
ub3rpyr0 <scott.wenzl@gmail.com>
2017-12-27 09:07:19 -0700

    add and complete my_xgboost R script. This includes xgboost.model created by running the script

---
+++ Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 49 - XGBoost/XGBoost/my_xgboost.R
@@ -0,0 +1,53 @@
+# XGBoost - feature scaling not necessary
+
+# Importing the dataset
+dataset = read.csv("Churn_Modelling.csv")
+dataset = dataset[4:14] # use this to select a subset of the data
+
+# Taking care of missing data
+# dataset$Age = ifelse(is.na(dataset$Age),
+#                      ave(dataset$Age, FUN=function(x) mean(x,na.rm=TRUE)),
+#                      dataset$Age)
+# dataset$Salary = ifelse(is.na(dataset$Salary),
+#                         ave(dataset$Salary, FUN=function(x) mean(x,na.rm=TRUE)),
+#                         dataset$Salary)
+
+# Encoding the categorical variables as factors
+dataset$Geography = as.numeric(factor(dataset$Geography,
+                                      levels = c("France","Spain","Germany"),
+                                      labels = c(1, 2, 3)))
+dataset$Gender = as.numeric(factor(dataset$Gender,
+                                   levels = c("Female","Male"),
+                                   labels = c(1, 2)))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Exited, SplitRatio=0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting XGBoost to the training set
+# install.packages("xgboost")
+library(xgboost)
+classifier = xgboost(data = as.matrix(training_set[-11]),
+                     label = training_set$Exited,
+                     nrounds = 10)
+
+# Applying k-Fold Cross Validation
+library(caret)
+folds = createFolds(training_set$Exited, k = 10)
+cv = lapply(folds, function(x) {
+  training_fold = training_set[-x,]
+  test_fold = training_set[x,]
+  classifier = xgboost(data = as.matrix(training_fold[-11]),
+                       label = training_fold$Exited,
+                       nrounds = 10)
+  y_pred = predict(classifier, newdata = as.matrix(test_fold[-11]))
+  y_pred = (y_pred >= 0.5) # convert to binary outcome since XGBoost returns probabilities
+  cm = table(test_fold[,11], y_pred)
+  accuracy = (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[2,2] + cm[1,2] + cm[2,1])
+  return(accuracy)
+})
+accuracy = mean(as.numeric(cv))
