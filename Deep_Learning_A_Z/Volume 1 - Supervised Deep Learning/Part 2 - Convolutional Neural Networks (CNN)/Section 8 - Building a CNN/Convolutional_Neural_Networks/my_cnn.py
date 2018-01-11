373bb21c261a939a0e91dfef63c5cca53cee00ab
ub3rpyr0 <scott.wenzl@gmail.com>
2018-01-11 15:18:49 -0700

    complete my_cnn py script, takes 24h to run on laptop

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/my_cnn.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/my_cnn.py
@@ -1,2 +1,56 @@
 # Convolutional Neural network
 
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+
+# Initializing the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3), activation = "relu"))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full Connection: make a classic ANN
+classifier.add(Dense(units = 128, activation = "relu"))
+classifier.add(Dense(units = 1, activation = "sigmoid"))
+
+# Compiling the CNN: choose a stochastic gradient descent algorithm, a loss function, and a performance metric
+# categorical cross entropy would be used in case of more than 2 outcomes
+classifier.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+
+# Part 2 - Fitting the CNN to the images
+# Use image augmentation to help prevent overfitting to the training set
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory("dataset/training_set",
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = "binary")
+
+test_set = test_datagen.flow_from_directory("dataset/test_set",
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = "binary")
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
